b508c092285d57eace362d48709f8bd2b8850f13
Ramish <agamatimtim@gmail.com>
2022-07-12 00:32:48 -0500

    b

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
+import store from './store/index';
 import './index.css';
 import App from './App';
-import {Provider} from "react-redux";
-import store from "./store/appStore";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><App/></Provider>);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
